9668dd4e4181989fe9bb7d8b842b5ee14cdae033
wiswan0607 <57123664+wiswan0607@users.noreply.github.com>
2020-01-12 16:06:11 +0700

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Break.Continue;
+
+/**
+ *
+ * @author apit
+ */
+public class NewClass {
+    public static void main(String[] arg){
+        for (int i = 0; i < 10; i++){
+            if (i==4){
+                break;
+            }
+            System.out.println (i);
+        }
+    }
+    
+}
---
+++ NewClass1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Break.Continue;
+
+/**
+ *
+ * @author apit
+ */
+public class NewClass1 {
+    public static void main(String[] arg){
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+    }
+}    
